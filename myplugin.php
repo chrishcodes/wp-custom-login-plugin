4f87e3e046d8b1549680b418e7263083a2b9203a
chrish <chrishcodes@gmail.com>
2020-01-22 00:01:12 -0800

    first commit

---
+++ myplugin.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Plugin Name: MyPlugin
+Description: Welcome to WordPress plugin development.
+Plugin URI:  https://plugin-planet.com/
+Author:      Christopher Hall
+Version:     1.0
+License:     GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 
+2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+with this program. If not, visit: https://www.gnu.org/licenses/
+*/
+
+// exit if file is called directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// if is admin 
+if ( is_admin() ) {
+	//include dependencies
+	require_once plugin_dir_path( __FILE__ ) . 'admin/admin-menu.php';
+	require_once plugin_dir_path( __FILE__ ) . 'admin/settings-page.php';
+
+}
